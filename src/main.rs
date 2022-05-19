0b99c6633f6f166193b0fb796bbd680743585d72
ashleyhen <1asdfah@gmail.com>
2022-05-19 10:55:03 -0400

    stamples added

--- src/main.rs
+++ src/main.rs
@@ -21,10 +21,10 @@ fn _test_transactionn(){
 	let seed= "1d454c6ab705f999d97e6465300a79a9595fb5ae1186ae20e33e12bea606c094";
 		let to_addr="tb1puma0fas8dgukcvhm8ewsganj08edgnm6ejyde3ev5lvxv4h7wqvqpjslxz";
 	
-	let client_with_schema=ClientWithSchema::new(P2TR::new(Some(seed.to_string())) );
+	let client_with_schema=ClientWithSchema::new(P2PWKh::new(Some(seed.to_string())) );
 	client_with_schema.print_balance(0,0);
 
-		client_with_schema.submit_tx(to_addr.to_string(), 800, 0, 0);
+		client_with_schema.submit_tx(to_addr.to_string(),1000, 0, 6);
 		
 }
 // seed, vec<derivation path>
