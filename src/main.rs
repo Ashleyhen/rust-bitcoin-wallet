0c30203376dac297f362eb30250a88ec4e4f6a7e
ashleyhen <hendrash@mail.gvsu.edu>
2022-05-25 23:36:17 -0400

    transaction spent

--- src/main.rs
+++ src/main.rs
@@ -21,11 +21,11 @@ fn _test_transactionn(){
 	let seed= "1d454c6ab705f999d97e6465300a79a9595fb5ae1186ae20e33e12bea606c094";
 		let to_addr="tb1p95xjusgkgh2zqhyr5q9hzwv607yc5dncnsastm9xygmmuu4xrcqs53468m";
 	
-	let client_with_schema=ClientWithSchema::new(P2TR::new(Some(seed.to_string()),0,0) );
+	let client_with_schema=ClientWithSchema::new(P2TR::new(Some(seed.to_string()),0,1) );
 	// let client_with_schema=ClientWithSchema::new(P2TR::new(Some(seed.to_string()),0,0) );
 	client_with_schema.print_balance(0,0);
 
-		client_with_schema.submit_tx(to_addr.to_string(),800);
+		// client_with_schema.submit_tx(to_addr.to_string(),800);
 		
 }
 // seed, vec<derivation path>
