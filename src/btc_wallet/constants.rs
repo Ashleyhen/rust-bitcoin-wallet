b8632f1cde4b55cf91dbb484336e2b402cd13ea6
ashleyhen <hendrash@mail.gvsu.edu>
2022-09-03 12:29:24 -0400

    remove deprecated btc_wallet version

--- src/btc_wallet/constants.rs
+++
@@ -1,5 +0,0 @@
-use bitcoin::secp256k1::constants;
-
-pub(crate) type Seed = [u8; constants::SECRET_KEY_SIZE];
-pub const NETWORK: bitcoin::Network = bitcoin::Network::Regtest;
-pub const TIP: u64 = 300;
