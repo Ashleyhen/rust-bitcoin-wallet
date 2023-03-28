4aa5c89766f3051c34ddfdc847526e9f4c2bc452
ashleyhen <1asdfah@gmail.com>
2023-03-28 16:44:56 -0400

    missing lnd_send_open_channel_request

--- src/lighting/lighting_demo.rs
+++ src/lighting/lighting_demo.rs
@@ -133,6 +133,72 @@ pub async fn clighting_sends_open_channel_request() {
     println!("open channel request {:#?}", open_channel_response);
 }
 
+
+
+#[tokio::test]
+pub async fn lnd_sends_open_channel_request() {
+    let mut lnd = Lnd::new().await;
+    let mut lightingd = Lightingd::new().await;
+    let get_info = lightingd.get_info().await;
+
+    let client = RegtestCall::init(
+        &vec!["bcrt1prnpxwf9tpjm4jll4ts72s2xscq66qxep6w9hf6sqnvwe9t4gvqasklfhyj"],
+        "my_wallet",
+        110,
+    );
+
+    thread::sleep(Duration::from_secs(2));
+
+    let lnd_to_lightind = lnd
+        .connect(get_info.clone().id, "10.5.0.5:19846".to_string())
+        .await;
+
+    println!("connect peer {:#?}", lnd_to_lightind);
+
+    client.mine(20);
+    let new_address = lnd.new_address(AddrType::TR).await;
+    P2TR::new(Some(SEED), &client).send(single_output_with_value(new_address.clone()));
+    client.mine(100);
+
+    println!(
+        "clighting and lnd channel multi-sig address \n {}",
+        new_address.clone()
+    );
+
+    thread::sleep(Duration::from_secs(3));
+
+    let open_channel_response = lnd
+        .open_channel(lightingd.get_info().await.id, Some(10000000))
+        .await;
+
+    println!("open channel request {:#?}", open_channel_response);
+
+    client.mine(20);
+
+    thread::sleep(Duration::from_secs(2));
+
+    let invoice = lightingd
+        .create_invoice(
+            1000,
+            "invoice from lightingd",
+            "payment description!",
+            Some(7200),
+        )
+        .await;
+
+    println!("print out invoice {:#?}", invoice);
+
+    let list_peer_request = lnd.list_peers().await.get_ref().clone();
+
+    println!("list peers {:#?}", list_peer_request);
+
+    println!("Testing layer 1 pay to tap root with key signature");
+}
+
+
+
+
+
 #[tokio::test]
 pub async fn lnd_list_peer() {
     let mut lnd = Lnd::new().await;
